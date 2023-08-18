b4943ebd47749317b3ac2df55b8097c908809a06
JiHyeok Heo <vndj1212@gmail.com>
2023-08-18 18:49:41 +0900

    Revert "Revert "Fmod""
    This reverts commit d763c839a9057d8ed1fd59dec65f8cb4e5bc9528.

---
+++ JNSEngine/Engine_SOURCE/jnsAudioListener.cpp
@@ -0,0 +1,44 @@
+#include "jnsAudioListener.h"
+#include "jnsTransform.h"
+#include "jnsGameObject.h"
+#include "jnsFmod.h"
+
+namespace jns
+{
+	AudioListener::AudioListener()
+		: Component(eComponentType::AudioListener)
+	{
+
+	}
+
+	AudioListener::~AudioListener()
+	{
+
+	}
+
+	void AudioListener::Initialize()
+	{
+
+	}
+
+	void AudioListener::Update()
+	{
+
+	}
+
+	void AudioListener::LateUpdate()
+	{
+		Transform* tr = GetOwner()->GetComponent<Transform>();
+		Vector3 pos = tr->GetPosition();
+		Vector3 foward = tr->Foward();
+		Vector3 up = tr->Up();
+
+		Vector3 vel = { 0.0f, 0.0f, 0.0f };
+		Fmod::Set3DListenerAttributes(&pos, &vel, &foward, &up);
+	}
+
+	void AudioListener::Render()
+	{
+
+	}
+}
---
+++ JNSEngine/Engine_SOURCE/jnsAudioListener.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "jnsComponent.h"
+
+
+namespace jns
+{
+	class AudioListener : public Component
+	{
+	public:
+		AudioListener();
+		~AudioListener();
+
+		virtual void Initialize() override;
+		virtual void Update() override;
+		virtual void LateUpdate() override;
+		virtual void Render() override;
+
+	private:
+	};
+}
